af94ef05caca5813b0a4bdcd82ea1e167320c977
Nkosinathi <nkosimkhonza5@gmail.com>
2026-05-03 05:13:32 +0200

    feat: register DailyEntry with django admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import DailyEntry
 
-# Register your models here.
+
+@admin.register(DailyEntry)
+class DailyEntryAdmin(admin.ModelAdmin):
+    list_display  = ['user', 'date', 'hours_coded', 'applications_sent', 'created_at']
+    list_filter   = ['user', 'date']
+    search_fields = ['user__username', 'notes']
+    ordering      = ['-date']
