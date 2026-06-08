763a9e97822fc2aff0926368de130c1ace36e465
Nkosinathi <nkosimkhonza5@gmail.com>
2026-06-08 16:51:08 +0200

    fix: exempt login endpoint from CSRF

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
+from rest_framework.authtoken.views import obtain_auth_token
+from django.views.decorators.csrf import csrf_exempt
+
 urlpatterns = [
     path('admin/',      admin.site.urls),
     path('api/',        include('tracker.urls')),
-    path('api/login/',  obtain_auth_token, name='api-login'),
+    path('api/login/',  csrf_exempt(obtain_auth_token), name='api-login'),
     path('',            TemplateView.as_view(template_name='base.html'), name='home'),
     path('login',       TemplateView.as_view(template_name='base.html'), name='login'),
     path('register',    TemplateView.as_view(template_name='base.html'), name='register'),
